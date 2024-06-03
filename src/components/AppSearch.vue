8a861311c315f45a71ff7109fa71319bd247bcc2
gianluigi7 <gianluigiferraioli7@gmail.com>
2024-06-03 20:22:16 +0200

    header

---
+++ src/components/AppSearch.vue
@@ -0,0 +1,32 @@
+<script>
+export default {
+    name: 'AppSearch'
+
+    
+}
+</script>
+
+<template>
+   <div class="container-search">
+
+       <input>
+       <input>
+       <button>Search</button>
+       <button>Reset</button>
+    </div>
+</template>
+
+<style scoped>
+ .container-search {
+    max-width: fit-content;
+    margin: 0 auto;
+}
+button {
+    background-color: orange;
+    padding: 6px 8px;
+    border-radius: 7px;
+    border: none;
+    margin: 0 6px;
+}
+
+</style>
---
+++ src/components/AppTitle.vue
@@ -0,0 +1,16 @@
+<script>
+export default {
+    name: 'AppTitle',
+};
+</script>
+
+<template>
+    <h1>Rick and Morty App</h1>
+</template>
+
+<style scoped>
+ h1 {
+    text-align: center;
+    padding: 30px;
+ }
+</style>
